f045345f20db4d8c856c3201efc5979f2078a995
beckybp <becky.pugh@hotmail.co.uk>
2023-03-30 13:54:32 +0100

    adding connection to listen file

--- listen.js
+++ listen.js
@@ -1,5 +1,23 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path: `.env`,
+});
+const mongoString = process.env.DATABASE_URL;
+
+import mongoose from "mongoose";
 
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
