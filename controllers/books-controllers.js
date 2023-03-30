320d1048f7cc241367487b40d6b3f2f10e234912
Hayden <haydenworden@outlook.com>
2023-03-30 15:36:51 +0100

    Delete single book endpoint + tests

--- controllers/books-controllers.js
+++ controllers/books-controllers.js
@@ -14,12 +14,12 @@ export const getBook = async (req, res, next) => {
     const bookData = await bookModels
       .findById(req.params.id)
       .setOptions({ sanitizeFilter: true });
-    !bookData ? res.status(404).send({msg:'Book not found.'}):null
-    return res.status(200).send({ book: bookData })
+    !bookData ? res.status(404).send({ msg: "Book not found" }) : null;
+    return res.status(200).send({ book: bookData });
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const postBook = async (req, res, next) => {
   try {
@@ -29,11 +29,23 @@ export const postBook = async (req, res, next) => {
       genre: req.body.genre,
       posted_by: req.body.posted_by,
       location: req.body.location,
-      location_description: req.body.location_description
-    })
-    const dataToSave = await newBook.save()
-    return res.status(201).send({book: dataToSave})
+      location_description: req.body.location_description,
+    });
+    const dataToSave = await newBook.save();
+    return res.status(201).send({ book: dataToSave });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const deleteBook = async (req, res, next) => {
+  try {
+    const bookData = await bookModels.findByIdAndDelete(req.params.id);
+
+    const deletedBook = await bookModels.findById(req.params.id);
+    bookData === null ? res.status(404).send({ msg: "Book not found" }) : null;
+    res.status(200).send({ msg: "Book deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
