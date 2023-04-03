38af1c50b926de200db546c01855554b87f28945
erinmd <erin817@gmail.com>
2023-04-03 16:19:41 +0100

    added authorization to postUser (hopefully)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,39 +1,45 @@
-import userModel from "../model/user.js";
+import userModel from '../model/user.js'
 
 export const getUsers = async (req, res) => {
   try {
-    const userData = await userModel.find();
-    return res.status(200).send({ users: userData });
+    const userData = await userModel.find()
+    return res.status(200).send({ users: userData })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export const getUser = async (req, res, next) => {
   try {
     const userData = await userModel
-      .findOne({firebase_id: req.params.id})
-      .setOptions({ sanitizeFilter: true });
+      .findOne({ firebase_id: req.params.id })
+      .setOptions({ sanitizeFilter: true })
     if (!userData) {
-      await Promise.reject({ status: 404, msg: "User Not Found" });
+      await Promise.reject({ status: 404, msg: 'User Not Found' })
     } else {
-      return res.status(200).send({ user: userData });
+      return res.status(200).send({ user: userData })
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export const postUser = async (req, res, next) => {
-  try {
-    const newUser = new userModel({
-      username: req.body.username,
-      firebase_id: req.body.firebase_id,
-      name: req.body.name,
-    });
-    const dataToSave = await newUser.save();
-    return res.status(201).send({ user: dataToSave });
-  } catch (error) {
-    next(error);
+  const auth = req.currentUser
+  if (auth) {
+    console.log('authorized!')
+    try {
+      const newUser = new userModel({
+        username: req.body.username,
+        firebase_id: req.body.firebase_id,
+        name: req.body.name
+      })
+      const dataToSave = await newUser.save()
+      return res.status(201).send({ user: dataToSave })
+    } catch (error) {
+      next(error)
+    }
   }
-};
+  console.log('not authorised')
+  return res.status(403).send({msg:'Not authorized'})
+}
