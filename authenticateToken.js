379529cf6b534ce8319fe943426f2d07a8579681
erinmd <erin817@gmail.com>
2023-04-03 16:54:55 +0100

    added createRequire

--- authenticateToken.js
+++ authenticateToken.js
@@ -1,24 +1,29 @@
-import { initializeApp } from "firebase-admin/app"
-import serviceAccount from './authkey.json'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const admin = require('firebase-admin')
+const serviceAccount = require('./authkey.json')
 
-initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://geobook2.firebaseapp.com'
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://geobook2.firebaseapp.com'
 })
 
-async function decodeIDToken(req, res, next) {
-    const header = req.headers?.authorization
-    if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
-        const idToken = req.headers.authorization.split('Bearer ')[1];
+async function decodeIDToken (req, res, next) {
+  const header = req.headers?.authorization
+  if (
+    header !== 'Bearer null' &&
+    req.headers?.authorization?.startsWith('Bearer ')
+  ) {
+    const idToken = req.headers.authorization.split('Bearer ')[1]
 
-        try {
-            const decodedToken = await admin.auth().verifyIdToken(idToken)
-            req['currentUser'] = decodedToken
-        } catch (err) {
-            console.log(err)
-        }
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken)
+      req['currentUser'] = decodedToken
+    } catch (err) {
+      console.log(err)
     }
-    next()
+  }
+  next()
 }
 
 export default decodeIDToken
